3fb000978b85fc22d9c237f8f9209805ebda04e4
yaelmagal10 <yaelmagal10@gmail.com>
2023-09-03 11:03:04 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 def run_server (server_ip, server_port):
   serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  serv.bind((server_port, server_ip))
+  serv.bind((server_ip, server_port))
   serv.listen(5)
   while True:
     conn, addr = serv.accept()

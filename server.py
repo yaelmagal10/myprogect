1eae6376df61dd4dfa1140d9dd3a6011f11d2c00
yaelmagal10 <yaelmagal10@gmail.com>
2023-09-03 10:37:04 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import socket
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('0.0.0.0', 8080))
-serv.listen(5)
-while True:
-  conn, addr = serv.accept()
-  from_client = ''
+def run_server (ip, port):
+  serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  serv.bind(('0.0.0.0', 8080))
+  serv.listen(5)
   while True:
-    data = conn.recv(4096)
-    if not data: break
-    from_client += data.decode('utf8')
-    print (from_client)
-    conn.send("I am SERVER\n".encode())
-  conn.close()
-print ('client disconnected and shutdown')
+    conn, addr = serv.accept()
+    from_client = ''
+    while True:
+      data = conn.recv(4096)
+      if not data: break
+      from_client += data.decode('utf8')
+      print (from_client)
+      conn.send("I am SERVER\n".encode())
+    conn.close()
+  print ('client disconnected and shutdown')

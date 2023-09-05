5cdda64b7cf3606a5c5e7f186ed1b7310ba927c7
yaelmagal10 <yaelmagal10@gmail.com>
2023-09-05 13:56:11 +0300

    the second asaiment1

--- client.py
+++ client.py
@@ -1,15 +1,24 @@
 import argparse
-import sys
 import socket
+import sys
 import struct
 
-
+'''
+we make a client that can wirte to the server encoded
+'''
 
 def send_data(server_ip, server_port, data):
+    '''
+    read from client and print it, can get from a number of clients
+    :param server_ip: the ip of the server
+    : type server_ip: int
+    :param server_port: the port of the server
+    :type server_port: int
+    '''
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((server_ip, server_port))
-    data_len=len(data)
-    data_len=struct.pack('<I', data_len)
+    data_len = len(data)
+    data_len = struct.pack('<I', data_len)
     client.sendall(data_len)
     client.sendall(data.encode())
     client.close()
@@ -17,6 +26,11 @@ def send_data(server_ip, server_port, data):
 
 
 def get_args():
+    '''
+    take the ip and port and massege from the user and make it usabule 
+    :return: the port and ip and massege
+    :rtype: argparse.ArgumentParser
+    '''
     parser = argparse.ArgumentParser(description='Send data to server.')
     parser.add_argument('server_ip', type=str,
                         help='the server\'s ip')
